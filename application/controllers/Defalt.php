0781e4c649681da9cdde4956e26efd1f64aa933f
Devair <devair.juca2013@gmail.com>
2019-04-03 12:21:30 -0300

    exemplo de insercao no banco.

--- application/controllers/Defalt.php
+++ application/controllers/Defalt.php
@@ -7,6 +7,8 @@ class Defalt extends CI_Controller{
     }
     
    public function index(){
+       $this->load->model('login/Insere');
+       $this->Insere->index();
        $this->load->view('login/index');
    }
 
