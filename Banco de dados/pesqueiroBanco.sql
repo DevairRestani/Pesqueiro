51ba6c4b7ac2004e73c5b51e79f195f5fecd2942
caio <caio.porto53@gmail.com>
2019-05-05 19:25:46 -0300

    footer arrmad

--- Banco de dados/pesqueiroBanco.sql
+++ Banco de dados/pesqueiroBanco.sql
@@ -1,1 +1,117 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.38-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Copiando estrutura do banco de dados para pesqueiro
+DROP DATABASE IF EXISTS `pesqueiro`;
+CREATE DATABASE IF NOT EXISTS `pesqueiro` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `pesqueiro`;
+
+-- Copiando estrutura para tabela pesqueiro.administrador
+DROP TABLE IF EXISTS `administrador`;
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `senha` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela pesqueiro.administrador: ~0 rows (aproximadamente)
+DELETE FROM `administrador`;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` (`id`, `email`, `login`, `senha`) VALUES
+	(1, 'teste@teste.com', 'testevaoldo', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela pesqueiro.comandasabertas
+DROP TABLE IF EXISTS `comandasabertas`;
+CREATE TABLE IF NOT EXISTS `comandasabertas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `administradorID` int(11) NOT NULL,
+  `horaabertura` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `estado` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `administradorID` (`administradorID`),
+  CONSTRAINT `comandasabertas_ibfk_1` FOREIGN KEY (`administradorID`) REFERENCES `administrador` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela pesqueiro.comandasabertas: ~2 rows (aproximadamente)
+DELETE FROM `comandasabertas`;
+/*!40000 ALTER TABLE `comandasabertas` DISABLE KEYS */;
+INSERT INTO `comandasabertas` (`id`, `nome`, `administradorID`, `horaabertura`, `estado`) VALUES
+	(2, 'teste', 1, '2019-05-03 20:24:07', 1),
+	(3, 'teste 2', 1, '2019-05-03 20:24:45', 1),
+	(4, 'teste 3', 1, '2019-05-03 20:24:49', 1);
+/*!40000 ALTER TABLE `comandasabertas` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela pesqueiro.comprasabertas
+DROP TABLE IF EXISTS `comprasabertas`;
+CREATE TABLE IF NOT EXISTS `comprasabertas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produtoID` int(11) NOT NULL,
+  `comandaID` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `produtoID` (`produtoID`),
+  KEY `comandaID` (`comandaID`),
+  CONSTRAINT `comprasabertas_ibfk_1` FOREIGN KEY (`produtoID`) REFERENCES `produtos` (`id`),
+  CONSTRAINT `comprasabertas_ibfk_2` FOREIGN KEY (`comandaID`) REFERENCES `comandasabertas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela pesqueiro.comprasabertas: ~0 rows (aproximadamente)
+DELETE FROM `comprasabertas`;
+/*!40000 ALTER TABLE `comprasabertas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comprasabertas` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela pesqueiro.estoque
+DROP TABLE IF EXISTS `estoque`;
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produtoID` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `dataUltimaAlteracao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `estoque_ibfk_1` (`produtoID`),
+  CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`produtoID`) REFERENCES `produtos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela pesqueiro.estoque: ~0 rows (aproximadamente)
+DELETE FROM `estoque`;
+/*!40000 ALTER TABLE `estoque` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estoque` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela pesqueiro.produtos
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `valor` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela pesqueiro.produtos: ~0 rows (aproximadamente)
+DELETE FROM `produtos`;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` (`id`, `nome`, `valor`) VALUES
+	(1, 'coca', 12.1),
+	(2, 'jaca', 1231);
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
 INSERT INTO administrador(email, login, senha) VALUES('admin@admin.com', 'admin@admin.com', SHA1('123456'));
+INSERT INTO administrador(email, login, senha) VALUES('caio@caio.com', 'logincaio', SHA1('senhacaio'));
--- application/views/comum/navBar.php
+++ application/views/comum/navBar.php
@@ -13,10 +13,13 @@
 </head>
 
 <style>
+
 .page-footer {
-    position: absolute;
-    bottom: 0;
+    margin-top:10px;
     width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;   
     background-color: #007bff;
     color: white;
 }
@@ -33,44 +36,43 @@ label{
 </style>
 
 <body>
+    
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="margin-bottom: 25px;">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="margin-bottom: 25px;">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="<?= base_url('home'); ?>">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url('estoque');?>">Estoque</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
-            </form>
-            <img class="lupa" src="assets/img/search.png" type="submit">
-        </div>
-    </nav>
-
-    <div class="container">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?= base_url('home'); ?>">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('estoque');?>">Estoque</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+                </form>
+                <img class="lupa" src="assets/img/search.png" type="submit">
+            </div>
+        </nav>
+<div class="container">
